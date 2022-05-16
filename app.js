d52d18d37e8d7f8b4704bd7eb80edfb2f3e0abc3
abutler911 <abutler911@gmail.com>
2022-05-16 08:50:10 -0600

    play button and format changes

---
+++ app.js
@@ -0,0 +1,9 @@
+function toggle() {
+    var trailer = document.querySelector(".trailer");
+    var video = document.querySelector("video");
+    trailer.classList.toggle("active");
+    video.pause();
+    video.currentTime = 0;
+  }
+
+  
