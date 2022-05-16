2e0185787b82ccede421266ec34f26ec436f4485
abutler911 <abutler911@gmail.com>
2022-05-16 12:55:37 -0600

    section styling changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function toggle() {
     var trailer = document.querySelector(".trailer");
     var video = document.querySelector("video");
     trailer.classList.toggle("active");
-    video.pause();
+    video.play();
     video.currentTime = 0;
   }
 
